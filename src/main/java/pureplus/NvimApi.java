0ab05d903b63973bd6c55e282fa27528ec16ad43
Yusuke SHIROYAMA <shiroyagiyuu@gmail.com>
2026-08-13 15:54:16 +0900

    refacter request api to add support Map

--- src/main/java/pureplus/NvimApi.java
+++ src/main/java/pureplus/NvimApi.java
@@ -6,6 +6,8 @@ import org.msgpack.core.MessagePacker;
 import java.io.OutputStream;
 import java.io.IOException;
 
+import java.util.Map;
+
 public class NvimApi
 {
     OutputStream  out;
@@ -16,38 +18,10 @@ public class NvimApi
         this.msgid = 0;
     }
 
-    public int uiAttach(int width, int height) throws IOException {
-        this.msgid++;
+    public void uiAttach(int width, int height) throws IOException {
+        Map   options = Map.of("ext_linegrid", true);
 
-        MessagePacker packer =
-                MessagePack.newDefaultPacker(out);
-
-        // [0, msgid, method, params]
-        packer.packArrayHeader(4);
-
-        // REQUEST
-        packer.packInt(0);
-
-        // msgid
-        packer.packInt(msgid);
-
-        // method
-        packer.packString("nvim_ui_attach");
-
-        // params = [width, height, options]
-        packer.packArrayHeader(3);
-
-        packer.packInt(width);
-        packer.packInt(height);
-
-        // {}
-        packer.packMapHeader(1);
-        packer.packString("ext_linegrid");
-        packer.packBoolean(true);
-
-        packer.flush();
-
-        return msgid;
+        request("nvim_ui_attach", width, height, options);
     }
 
     public void input(String keycode) throws IOException {
@@ -58,7 +32,32 @@ public class NvimApi
         request("nvim_ui_try_resize", width, height);
     }
 
-    public void request(String method, Object... params) throws IOException {
+    private void packMap(MessagePacker packer, Map map) throws IOException {
+        packer.packMapHeader(map.size());
+
+        for (Object keyobj : map.keySet()) {
+            String key = "";
+            if (keyobj instanceof String) {
+                key = (String)keyobj;
+                packer.packString(key);
+            
+                Object value = map.get(key);
+
+                if (value instanceof Integer) {
+                    packer.packInt((int)value);
+                } else if (value instanceof Boolean) {
+                    packer.packBoolean((boolean)value);
+                } else if (value instanceof String) {
+                    packer.packString((String)value);
+                }
+            }
+            else {
+                throw new ClassCastException("Map key nees String. key is "+keyobj.getClass());
+            }
+        }
+    }
+
+    private void request(String method, Object... params) throws IOException {
         this.msgid++;
 
         MessagePacker packer = MessagePack.newDefaultPacker(out);
@@ -85,8 +84,11 @@ public class NvimApi
             else if (params[i] instanceof String) {
                 packer.packString((String)params[i]);
             }
+            else if (params[i] instanceof Map) {
+                packMap(packer, (Map)params[i]);
+            }
             else {
-                System.out.println("Not Supported Now");
+                throw new ClassCastException("Not Supported Now.. class is "+params[i].getClass());
             }
         }
 
